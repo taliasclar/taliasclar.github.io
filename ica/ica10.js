ad048e96257e1bb72f13127e43574d3f8369343a
taliasclar <talia.sclar@colorado.edu>
2025-10-22 11:19:44 -0600

    x

---
+++ ica/ica10.js
@@ -0,0 +1,47 @@
+let triviaBtn = document.querySelector("#js-new-quote").addEventListener('click', newTrivia);
+
+let answerBtn = document.querySelector('#js-tweet');
+addEventListener('click', newAnswer);
+
+let current = {
+    question: "",
+    answer: "",
+}
+
+const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
+
+async function newTrivia() {
+    //console.log("sucess")
+
+    try {
+        const response = await fetch(endpoint);
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        const json = await response.json();
+        // console.log(json);
+        displayTrivia(json["question"])
+        current.question = json["question"];
+        current.answer = json["answer"];
+        // console.log(current.question);
+        // console.log(current.asnwer);
+
+    } catch (err) {
+        console.log(err)
+        alert('Failed to get new trivia');
+    }
+}
+
+function displayTrivia(question) {
+    const questionText = document.querySelector('#js-quote-text');
+    const answerText = document.querySelector('#js-answer-text');
+    questionText.textContent = question;
+    answerText.textContent = "";
+}
+
+function newAnswer() {
+    const answerText = document.querySelector('#js-answer-text');
+    answerText.textContent = current.answer;
+}
+
+newTrivia()
